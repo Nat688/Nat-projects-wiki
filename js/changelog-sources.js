ef1f74ec931c37024a727791d325f4c97e2d91b0
Nat_68 <nat.68.ng@gmail.com>
2026-09-16 16:54:02 +0200

    Add changelog sources for various features

---
+++ js/changelog-sources.js
@@ -0,0 +1,13 @@
+window.CHANGELOG_SOURCES = [
+	{ slug: "c-to-gun",            id: "coals-to-gunpowder",  badge: "Coals to Gunpowder" },
+	{ slug: "recyclings",          id: "recycling",           badge: "Recycling" },
+	{ slug: "villager-heal-golem", id: "villager-heal-golem", badge: "Villager Heal Golem" },
+	{ slug: "better-crafts",       id: "better-crafts",       badge: "Better Crafts" },
+	{ slug: "speed-paths",         id: "speed-paths",         badge: "Speed Paths" },
+	{ slug: "armorer-heal-golem",  id: "armorer-heal-golem",  badge: "Armorer Heal Golem" },
+	{ slug: "lava-source",         id: "lava-source",         badge: "Lava Source" },
+	{ slug: "better_furnaces",     id: "better-furnaces",     badge: "Better Furnaces" },
+	{ slug: "cookies-clicker",     id: "cookie-clicker",      badge: "Cookie Clicker" },
+	{ slug: "villagers-tasks",     id: "villagers-tasks",     badge: "Villagers Tasks" },
+	{ slug: "better-brewings",     id: "better-brewings",     badge: "Better Brewings" },
+];
