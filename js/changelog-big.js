dde0ff677159d0651f99d405986c18ad720e2026
Nat_68 <nat.68.ng@gmail.com>
2026-09-18 21:24:51 +0200

    Add changelog for various versions

---
+++ js/changelog-big.js
@@ -0,0 +1,13 @@
+window.CHANGELOG_BIG_VERSIONS = {
+	"armorer-heal-golem": ["1.0.0"],
+	"better-brewings": ["1.0.0", "1.0.0snapshot-1"],
+	"better-crafts": ["2.0.0snapshot-1", "0.1.0"],
+	"better-furnaces": ["1.0.0", "1.0.0snapshot-1"],
+	"coals-to-gunpowder": ["2.10.0", "2.1.0", "2.0.0", "1.0.0", "0.2.0", "0.1.0"],
+	"cookie-clicker": ["1.0.0snapshot-23", "1.0.0snapshot-20", "1.0.0snapshot-19", "1.0.0snapshot-18", "1.0.0snapshot-3", "1.0.0snapshot-1"],
+	"lava-source": ["1.1.0", "1.0.0"],
+	"recycling": ["5.0.0", "4.0.0", "3.0.0", "2.0.0", "1.7.0", "1.5.0", "1.3.0", "1.0.0", "0.1.0"],
+	"speed-paths": ["3.0.0", "2.0.0"],
+	"villager-heal-golem": ["1.0.0snapshot-1"],
+	"villagers-tasks": ["1.0.0snapshot-2", "1.0.0snapshot-1"],
+};
